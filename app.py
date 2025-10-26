a60da16134a559e150b31fbf1d870a8299a923a0
Kazz02 <jesse.partonen@edu.turkuamk.fi>
2025-10-26 19:06:54 +0200

    Test route is working as intended

---
+++ app.py
@@ -0,0 +1,40 @@
+import requests
+from flask import Flask, jsonify, request
+from flask_cors import CORS
+from config import VirusTotal_key, AbuseIPDB_key
+
+# Creation of Flask app + CORS to allow frontend to talk to backend
+app = Flask(__name__)
+CORS(app)
+
+# Test route
+@app.route('/')
+def hello():
+    return "First check!"
+
+# API endpoint
+@app.route('/api/check-domain', methods=['POST'])
+def check_domain():
+    domain_to_check = request.json.get('domain')
+
+    if not domain_to_check:
+        return jsonify({'error': 'No domain provided'}), 400
+    
+    # Call VirusTotal API
+    url = f"https://www.virustotal.com/api/v3/domains/{domain_to_check}"
+    headers = {
+        "x-apikey": VirusTotal_key
+    }
+
+    # VirusTotal API request and response handling
+    try:
+        response = requests.get(url, headers = headers)
+        response.raise_for_status() # Raises an error if bad response
+        return jsonify(response.json())
+    except requests.exceptions.HTTPError as http_err:
+        return jsonify({'error': f'HTTP error occurred: {http_err}'}), response.status_code
+    except requests.exceptions.RequestException as req_err:
+        return jsonify({'error': f'Request error occurred: {req_err}'}), 500
+    
+if __name__ == '__main__':
+    app.run(debug=True, port=5000)
